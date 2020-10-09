6228a993f2d067bf5cbddd3a56599f03846d86a5
Joschua Schneider <joschua-schneider@web.de>
2020-10-09 15:47:02 +0200

    initial: add global types

---
+++ interfaces/index.d.ts
@@ -0,0 +1,11 @@
+export type FrontMatter = {
+  title: string
+  summary: string
+  date: string
+  image?: string
+  unsplash?: string
+  __resourcePath: string
+  readingTime: {
+    text: string
+  }
+}
