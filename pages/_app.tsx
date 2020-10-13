d4dfc2d1505a0b4b3ff4bb3258cd73d91b1cc3ea
Joschua Schneider <joschua-schneider@web.de>
2020-10-13 16:08:46 +0200

    feat: include new project and change to grid, add seo data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import type { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
 import 'styles/global.css'
 import 'styles/prism-theme.css'
+import { DefaultSeo } from 'next-seo'
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <DefaultSeo titleTemplate="%s - Developer, CS Student from Germany" />
+    <Component {...pageProps} />
+  </>
+)
 
 export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,25 @@ import { GetStaticProps } from 'next'
 import { queryGithubAPI } from 'utils/github-api'
 
 export const getStaticProps: GetStaticProps = async () => {
-  const featuredRepository = await queryGithubAPI(`#graphql
+  const repos = await queryGithubAPI(`#graphql
     query {
       viewer {
-        repository(name: "use-error-boundary") {
+        repoUseErrorBoundary: repository(name: "use-error-boundary") {
+          name
+          nameWithOwner
+          description
+          stargazerCount
+          openIssues: issues(states: [OPEN]) {
+            totalCount
+          }
+          closedIssues: issues(states: [CLOSED]) {
+            totalCount
+          }
+          primaryLanguage {
+            name
+          }
+        }
+        repoSnapcraft: repository(name: "snapcraft") {
           name
           nameWithOwner
           description
@@ -33,16 +48,22 @@ export const getStaticProps: GetStaticProps = async () => {
 
   return {
     props: {
-      featuredRepository: featuredRepository.data.viewer.repository,
+      repos: [
+        repos.data.viewer.repoUseErrorBoundary,
+        repos.data.viewer.repoSnapcraft,
+      ],
     },
   }
 }
 
-const Index = ({ featuredRepository }: { featuredRepository: any }) => (
+const Index = ({ repos }: { repos: any[] }) => (
   <Layout>
-    <NextSeo title="Joschua Schneider - Developer, CS Student from Germany" />
+    <NextSeo
+      title="Joschua Schneider"
+      description="Developer and CS Student from Germany, creating modern Web-Applications, tooling and learing resources."
+    />
     <Container className="my-8">
-      <Heading level="h1" className="mb-1">
+      <Heading level="h1" className="mb-4">
         Hey, I'm Joschua
       </Heading>
       <Text>
@@ -53,7 +74,7 @@ const Index = ({ featuredRepository }: { featuredRepository: any }) => (
       </Text>
     </Container>
     <Container className="my-12">
-      <Heading level="h2" className="mb-4">
+      <Heading level="h2" className="mb-6">
         Top Skills
       </Heading>
       <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
@@ -77,45 +98,47 @@ const Index = ({ featuredRepository }: { featuredRepository: any }) => (
           since={2019}
           className="col-span-1 text-indigo-700 bg-indigo-100"
         />
-        <button className="col-span-1 py-3 font-semibold text-center text-indigo-700 bg-indigo-100 bg-opacity-25 rounded-md hover:bg-opacity-50 md:col-span-3">
-          Browse all...
-        </button>
       </div>
     </Container>
     <Container className="my-12">
-      <Heading level="h2" className="mb-4">
+      <Heading level="h2" className="mb-6">
         OpenSource
       </Heading>
-      <div className="grid grid-cols-1">
-        <div className="px-6 py-4 bg-indigo-100 bg-opacity-25 border border-indigo-100 rounded-md">
-          <a
-            href={`https://github.com/${featuredRepository.nameWithOwner}`}
-            className="block mb-1 text-lg font-semibold text-indigo-700 hover:underline"
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+        {repos.map((repo) => (
+          <div
+            key={repo.name}
+            className="px-6 py-4 border border-indigo-100 rounded-md"
           >
-            {featuredRepository.name}
-          </a>
-          <p className="mb-2 text-gray-800">{featuredRepository.description}</p>
-          <p className="text-gray-700">
-            <span className="mr-4 text-gray-800">
-              <svg
-                className="inline-flex h-4 mb-px"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                />
-              </svg>{' '}
-              {featuredRepository.stargazerCount}
-            </span>
-            {featuredRepository.primaryLanguage.name}
-          </p>
-        </div>
+            <a
+              href={`https://github.com/${repo.nameWithOwner}`}
+              className="block mb-1 text-lg font-semibold text-indigo-700 hover:underline"
+            >
+              {repo.name}
+            </a>
+            <p className="mb-4 text-gray-800">{repo.description}</p>
+            <div className="flex text-gray-700">
+              <div className="mr-4 text-gray-800">
+                <svg
+                  className="inline-flex h-4 mb-px"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                  />
+                </svg>{' '}
+                {repo.stargazerCount}
+              </div>
+              <div className="ml-auto">{repo.primaryLanguage.name}</div>
+            </div>
+          </div>
+        ))}
       </div>
     </Container>
   </Layout>
