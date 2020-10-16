c76acbec21bfe13670a83da66653748a5a422aab
Joschua Schneider <joschua-schneider@web.de>
2020-10-16 11:14:20 +0200

    chore: remove unused href prop

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -40,10 +40,7 @@ const Layout = ({ children }: Props) => (
             </a>
           </Link>
           <Link href="/">
-            <a
-              href="#"
-              className="px-4 py-2 rounded hover:bg-gray-100 dark:hover:bg-gray-900"
-            >
+            <a className="px-4 py-2 rounded hover:bg-gray-100 dark:hover:bg-gray-900">
               Home
             </a>
           </Link>
